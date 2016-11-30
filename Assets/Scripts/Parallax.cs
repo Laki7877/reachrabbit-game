fe9991e49f6420654edfcdb5cdf16cc13508d416
Poon Wu <poon.wuthi@gmail.com>
2016-11-30 12:24:18 +0700

    Added more stuff

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Parallax : MonoBehaviour {
+
+	private Camera mainCamera;
+	private int screenWidth;
+	private int screenHeight;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	void Awake () {
+		mainCamera = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<Camera> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
