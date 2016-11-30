a2af3bd3fffbf87550639a66132b2d6966ae9ffd
Poon Wu <poon.wuthi@gmail.com>
2016-11-30 13:10:43 +0700

    Fixed scene

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -3,20 +3,30 @@ using System.Collections;
 
 public class Parallax : MonoBehaviour {
 
+	public Vector3 direction;
+
 	private Camera mainCamera;
 	private int screenWidth;
 	private int screenHeight;
 
 	// Use this for initialization
 	void Start () {
+		
 	}
 
 	void Awake () {
+		screenWidth = Screen.width;
+		screenHeight = Screen.height;
 		mainCamera = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<Camera> ();
+	}
+
+	void OnDrawGizmos() {
+		Gizmos.color = Color.green;
+		Gizmos.DrawLine (this.transform.position, this.transform.position + direction.normalized*5);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+		
+	}	
 }
