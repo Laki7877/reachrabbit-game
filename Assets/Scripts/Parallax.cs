93fc00029babe1ac3a728ebf13b867252a77d825
Poon Wu <poon.wuthi@gmail.com>
2016-11-30 13:55:09 +0700

    Updated parallaxing

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -3,15 +3,18 @@ using System.Collections;
 
 public class Parallax : MonoBehaviour {
 
+	public float speed;
+	public float tileSizeX;
+	public float debugSpeed;
 	public Vector3 direction;
-
+	private Vector3 startPosition;
 	private Camera mainCamera;
 	private int screenWidth;
 	private int screenHeight;
 
 	// Use this for initialization
 	void Start () {
-		
+		startPosition = this.transform.position;
 	}
 
 	void Awake () {
@@ -22,11 +25,13 @@ public class Parallax : MonoBehaviour {
 
 	void OnDrawGizmos() {
 		Gizmos.color = Color.green;
-		Gizmos.DrawLine (this.transform.position, this.transform.position + direction.normalized*5);
+		Gizmos.DrawLine (this.transform.position, this.transform.position + direction.normalized*2);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		float newPosition = Mathf.Repeat(Time.time * speed, tileSizeX);
+		debugSpeed = newPosition;
+		transform.position = startPosition + direction.normalized * debugSpeed;
 	}	
 }
