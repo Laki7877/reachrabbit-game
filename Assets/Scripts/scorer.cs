e962418ac0aad8d5d493ce919ef77e88fa25498f
Pat Sabpisal <ssabpisa@me.com>
2016-12-06 16:10:04 +0700

    Burger

---
+++ Assets/Scripts/scorer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class scorer : MonoBehaviour {
+
+	public Text text;
+	float meinScore = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void FixedUpdate(){
+		meinScore += 0.01f;
+		text.text = Mathf.Round (meinScore) + " m";
+	}
+}
