5907fe70a35f0c84003da3e200787d0dc18158ea
Poon Wu <poon.wuthi@gmail.com>
2016-12-06 14:20:22 +0700

    Added animator and physics

---
+++ Assets/Scripts/RabbitCharacterController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RabbitCharacterController : MonoBehaviour {
+	public float jumpingForce = 250.0f;
+	private Rigidbody2D body;
+	private Animator animator;
+
+	// Use this for initialization
+	void Awake () {
+		body = this.GetComponent<Rigidbody2D> ();
+		animator = this.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D o) {
+		if (o.collider.tag == "Hill") {
+			animator.SetBool ("IsRun", true);
+			animator.SetBool ("IsGrounded", true);
+		}
+	}
+
+	public void Jump() {
+		animator.SetBool ("IsGrounded", false);
+		body.AddForce(new Vector2(0f, jumpingForce));
+	}
+}
---
+++ Assets/Scripts/RabbitUserControl.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RabbitUserControl : MonoBehaviour {
+
+	RabbitCharacterController controller;
+	// Use this for initialization
+	void Awake () {
+		controller = this.GetComponent<RabbitCharacterController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate() {
+		
+		if ((Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Began) ||  Input.GetButtonDown("Jump")) {
+			controller.Jump ();
+		}	
+	}
+}
